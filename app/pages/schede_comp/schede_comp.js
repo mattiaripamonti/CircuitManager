01c32082fefe9116a7ec9661728bacb90280b5fd
Mattia Ripamonti <Mattia Ripamonti>
2015-11-26 22:04:11 +0100

    Vista schede compilate -- da completare

--- app/pages/schede_comp/schede_comp.js
+++ app/pages/schede_comp/schede_comp.js
@@ -23,5 +23,21 @@ angular.module('CircuitManager.schede_comp', ['firebase.utils', 'ngRoute'])
                 console.log("Error:", error);
             });
 
+        $scope.expand_scheda = function(scheda){
+            $scope.scheda_detail = scheda;
+            $scope.scheda_exs = [];
+
+            angular.forEach($scope.scheda_detail.id_es, function(value, key) {
+                var exs_ref = fbutil.ref('web/ct/esercizi/'+value.id);
+                var exs_array = $firebaseArray(exs_ref);
+                //TODO normalizzare la vista esercizio prima del push
+                $scope.scheda_exs.push(exs_array)
+            });
+
+            console.log($scope.scheda_exs)
+
+
+
+        }
 
     }]);
